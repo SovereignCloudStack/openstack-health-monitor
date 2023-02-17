4d773e331d1e962eba9fd4dfe20805b25005d352
Ralf Heiringhoff <ralf.heiringhoff@plusserver.com>
2023-02-17 04:31:43 +0100

    apply shfmt fixes
    ```bash
    shfmt --write --language-dialect bash --indent 4 --binary-next-line --case-indent --space-redirects --func-next-line run*.sh
    ```
    
    Signed-off-by: Ralf Heiringhoff <ralf.heiringhoff@plusserver.com>

--- run_pluspco.sh
+++ run_pluspco.sh
@@ -46,30 +46,30 @@ NOTE_EMAIL_ADDRESSES="scs@garloff.de"
 export ALARM_EMAIL_ADDRESSES NOTE_EMAIL_ADDRESSES
 
 # Terminate early on auth error
-openstack server list >/dev/null
+openstack server list > /dev/null
 
 # Find Floating IPs
 FIPLIST=""
 FIPS=$(openstack floating ip list -f value -c ID)
 for fip in $FIPS; do
-	FIP=$(openstack floating ip show $fip | grep -o "APIMonitor_[0-9]*")
-	if test -n "$FIP"; then FIPLIST="${FIPLIST}${FIP}_
+    FIP=$(openstack floating ip show $fip | grep -o "APIMonitor_[0-9]*")
+    if test -n "$FIP"; then FIPLIST="${FIPLIST}${FIP}_
 "; fi
 done
 FIPLIST=$(echo "$FIPLIST" | grep -v '^$' | sort -u)
 # Cleanup previous interrupted runs
-SERVERS=$(openstack server  list | grep -o "APIMonitor_[0-9]*_" | sort -u)
-VOLUMES=$(openstack volume  list | grep -o "APIMonitor_[0-9]*_" | sort -u)
+SERVERS=$(openstack server list | grep -o "APIMonitor_[0-9]*_" | sort -u)
+VOLUMES=$(openstack volume list | grep -o "APIMonitor_[0-9]*_" | sort -u)
 NETWORK=$(openstack network list | grep -o "APIMonitor_[0-9]*_" | sort -u)
 LOADBAL=$(openstack loadbalancer list | grep -o "APIMonitor_[0-9]*_" | sort -u)
-ROUTERS=$(openstack router  list | grep -o "APIMonitor_[0-9]*_" | sort -u)
+ROUTERS=$(openstack router list | grep -o "APIMonitor_[0-9]*_" | sort -u)
 SECGRPS=$(openstack security group list | grep -o "APIMonitor_[0-9]*_" | sort -u)
 echo CLEANUP: FIPs $FIPLIST Servers $SERVERS Volumes $VOLUMES Networks $NETWORK LoadBalancers $LOADBAL Routers $ROUTERS SecGrps $SECGRPS
 for ENV in $FIPLIST; do
-  echo "******************************"
-  echo "CLEAN $ENV"
-  bash ./api_monitor.sh -o -T -q -c CLEANUP $ENV
-  echo "******************************"
+    echo "******************************"
+    echo "CLEAN $ENV"
+    bash ./api_monitor.sh -o -T -q -c CLEANUP $ENV
+    echo "******************************"
 done
 TOCLEAN=$(echo "$SERVERS
 $VOLUMES
@@ -79,15 +79,14 @@ $ROUTERS
 $SECGRPS
 " | grep -v '^$' | sort -u)
 for ENV in $TOCLEAN; do
-  echo "******************************"
-  echo "CLEAN $ENV"
-  #bash ./api_monitor.sh -o -T -q -LL -c CLEANUP $ENV
-  bash ./api_monitor.sh -o -q -LL -c CLEANUP $ENV
-  echo "******************************"
+    echo "******************************"
+    echo "CLEAN $ENV"
+    #bash ./api_monitor.sh -o -T -q -LL -c CLEANUP $ENV
+    bash ./api_monitor.sh -o -q -LL -c CLEANUP $ENV
+    echo "******************************"
 done
 
 #bash ./api_monitor.sh -c -x -d -n 8 -l last.log -e $EMAIL_PARAM -S -i 9
 #exec api_monitor.sh -o -C -D -N 2 -n 8 -s -e sender@domain.org "$@"
 #exec ./api_monitor.sh -O -C -D -N 2 -n 8 -s -L -b -B -a 2 -t -T -R "$@"
 exec ./api_monitor.sh -O -C -D -N 2 -n 8 -s -LL -b -B -a 2 -t -T -R -S plus-pco "$@"
-
