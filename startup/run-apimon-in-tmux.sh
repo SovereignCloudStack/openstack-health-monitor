845df7a4fb0f8c202424d394fd9685a39cf53ac5
Kurt Garloff <kurt@garloff.de>
2024-02-25 20:29:32 +0100

    Fix OS_CACERT (cosmetic, as it was commented out).
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- startup/run-apimon-in-tmux.sh
+++ startup/run-apimon-in-tmux.sh
@@ -6,9 +6,9 @@ tmux start-server
 tmux new-session -d -s $session -n apimon
 tmux new-window -t $session:1 -n shell
 tmux send-keys "cd ~/openstack-health-monitor; export OS_CLOUD=$OS_CLOUD" C-m
-#tmux send-keys "export OS_CLOUD=$OS_CACERT" C-m
+#tmux send-keys "export OS_CACERT=$OS_CACERT" C-m
 tmux select-window -t $session:0
 tmux send-keys "cd ~/openstack-health-monitor; export OS_CLOUD=$OS_CLOUD" C-m
-#tmux send-keys "export OS_CLOUD=$OS_CACERT" C-m
+#tmux send-keys "export OS_CACERT=$OS_CACERT" C-m
 tmux send-keys "./run_in_loop.sh" C-m
 
