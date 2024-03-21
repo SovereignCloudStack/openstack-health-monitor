3694be4c40894c5b21cfa3e352d3bca2e37ee3ea
Kurt Garloff <kurt@garloff.de>
2024-03-21 09:15:26 +0000

    Implement support for VOLUMETYPE env variable.
    This will be passed to openstack server create --block-device (volume_type=)
    and to openstack volume create --type=. Allows to select non default
    volume type, to test e.g. an encrypted storage class LUKS.
    
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- api_monitor.sh
+++ api_monitor.sh
@@ -2057,8 +2057,8 @@ createJHVols()
 {
   if test -n "$NOJHVOL"; then return 0; fi
   JVOLSTIME=()
-  if test -n "$VOLUMETYPE"; then VOLTP="--volume-type $VOLUMETYPE"; else unset VOLTP; fi
-  createResources $NOAZS VOLSTATS JHVOLUME NONE NONE JVOLSTIME id $CINDERTIMEOUT cinder create --image-id $JHIMGID --availability-zone \${VAZS[\$VAZN]} $VOLTP --name ${RPRE}RootVol_JH\$no $JHVOLSIZE
+  if test -n "$VOLUMETYPE"; then VOLTP="--volume-type=$VOLUMETYPE"; else unset VOLTP; fi
+  createResources $NOAZS VOLSTATS JHVOLUME NONE NONE JVOLSTIME id $CINDERTIMEOUT cinder create --image-id $JHIMGID --availability-zone \${VAZS[\$VAZN]} --name ${RPRE}RootVol_JH\$no $VOLTP $JHVOLSIZE
 }
 
 # STATNM RSRCNM CSTAT STIME PROG1 PROG2 FIELD COMMAND
@@ -2080,8 +2080,8 @@ createVols()
 {
   if test -n "$BOOTFROMIMAGE"; then return 0; fi
   VOLSTIME=()
-  if test -n "$VOLUMETYPE"; then VOLTP="--volume-type $VOLUMETYPE"; else unset VOLTP; fi
-  createResources $NOVMS VOLSTATS VOLUME NONE NONE VOLSTIME id $CINDERTIMEOUT cinder create --image-id $IMGID --availability-zone \${VAZS[\$VAZN]} $VOLTP --name ${RPRE}RootVol_VM\$no $VOLSIZE
+  if test -n "$VOLUMETYPE"; then VOLTP="--volume-type=$VOLUMETYPE"; else unset VOLTP; fi
+  createResources $NOVMS VOLSTATS VOLUME NONE NONE VOLSTIME id $CINDERTIMEOUT cinder create --image-id $IMGID --availability-zone \${VAZS[\$VAZN]} --name ${RPRE}RootVol_VM\$no $VOLTP $VOLSIZE
 }
 
 # STATNM RSRCNM CSTAT STIME PROG1 PROG2 FIELD COMMAND
