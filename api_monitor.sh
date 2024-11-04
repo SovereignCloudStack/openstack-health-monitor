9a59f20ee6362d4e3fa01fb962d0b86e0d7639f0
Kurt Garloff <kurt@garloff.de>
2024-11-04 16:03:06 +0100

    Add up latencies of all percentiles > 10ms.
    The way fio reports it, the 10ms value is not all latencies above
    10ms, but just the ones b/w 10ms and the next segment (20ms). So
    we need to add up all values starting with 10ms and above.
    
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- api_monitor.sh
+++ api_monitor.sh
@@ -3421,7 +3421,9 @@ EOT
       if echo "$BENCH" | grep 'test:' >/dev/null 2>&1; then
 	READ=$(echo "$BENCH" | grep '  read:')
 	WRITE=$(echo "$BENCH" | grep '  write:')
-	LAT=$(echo "$BENCH" | grep '  lat (msec)' | grep ', 10=' | sed 's/^.*, 10=\([0-9\.]*\)%.*$/\1/')
+	LAT=$(echo "$BENCH" | grep '  lat (msec)' | grep '\([ ,]10=\|[, ]100=\)' | sed -e 's@^.*[, ]100\{0,1\}=@@' -e 's@%, [0-9]*=@+@g' -e 's@%$@+@')
+	LAT="0+${LAT}"
+	LAT=$(echo ${LAT%+} | bc -l)
 	RIOPS=$(echo "$READ" | sed 's/^.*IOPS=\([0-9]*\),.*$/\1/')
 	WIOPS=$(echo "$WRITE" | sed 's/^.*IOPS=\([0-9]*\),.*$/\1/')
 	RBW=$(echo "$READ" | sed 's/^.*BW=\([0-9\.]*\)MiB.*$/\1/')
