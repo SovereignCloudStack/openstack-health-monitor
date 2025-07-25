8d416263c2af968ce571f04cb6fd498a627fb3ff
Kurt Garloff <kurt@garloff.de>
2025-07-25 12:44:51 +0000

    Start wrappers for artcodix.
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

---
+++ run_in_loop_artcodix.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# run_in_loop_pluspco.sh
+#export WAITLB=60
+rm stop-os-hm 2>/dev/null
+while true; do
+	./run_artcodix.sh -s -i 200
+	if test -e stop-os-hm; then break; fi
+	echo -n "Hit ^C to abort ..."
+	sleep 15
+	echo
+done
+
